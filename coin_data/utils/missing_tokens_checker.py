659c22470ec542ccab0d9359a1c3ac44c4331d49
Hju Kneyck Flores <hjucode@gmail.com>
2025-02-18 20:58:04 +0800

    chore: add missing tokens checker utility to identify tokens absent in results

---
+++ coin_data/utils/missing_tokens_checker.py
@@ -0,0 +1,19 @@
+import polars as pl
+
+from coin_data import logger
+
+activities_path = ""
+results_path = ""
+
+activities_df = pl.read_csv(activities_path)
+results_df = pl.read_csv(results_path)
+
+# Extract unique token addresses from both datasets
+activities_tokens = set(activities_df["TokenAddress"].to_list())
+results_tokens = set(results_df["mint"].to_list())
+
+# Find missing tokens
+missing_tokens = activities_tokens - results_tokens
+missing_tokens_df = pl.DataFrame({"Missing Tokens": list(missing_tokens)})
+
+logger.info(f"Missing tokens: {missing_tokens_df}")
